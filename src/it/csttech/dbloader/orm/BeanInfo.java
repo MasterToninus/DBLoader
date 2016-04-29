bb46c24b95f8d19c3fa0a56a3e0764f58f0f4650
Tony <toninus.lcm@gmail.com>
2016-04-29 17:57:53 +0200

    BeanInfo constructor using annotations.

--- src/it/csttech/dbloader/orm/BeanInfo.java
+++ src/it/csttech/dbloader/orm/BeanInfo.java
@@ -3,15 +3,20 @@ package it.csttech.dbloader.orm;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Constructor;
-import java.util.SortedSet;
+
 import java.util.*;
 import java.lang.annotation.Annotation;
-import java.util.Collections;
-import java.util.Comparator;
+
 import it.csttech.dbloader.entities.Getter;
 import it.csttech.dbloader.entities.Setter;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class BeanInfo{
+
+  static final Logger log = LogManager.getLogger(BeanInfo.class.getName());
+
   private final Class<?> clazz;
   private final String clazzName;
   private  HashMap<String,Method> getters;
@@ -22,13 +27,19 @@ public class BeanInfo{
    * @param  clazz [description]
    * @return       [description]
    */
-  public BeanInfo(Class<?> clazz){
+  public BeanInfo(Class<?> clazz) {
     this.clazz = clazz;
     this.clazzName = clazz.getName();
     Method[] allMethods = clazz.getDeclaredMethods();
     Field[] allFields = clazz.getDeclaredFields();
-    fillMethods(allFields, allMethods);
-	fillMethods(allFields);
+    //fillMethods(allFields, allMethods);
+	  try{
+      fillMethods(allFields);
+    } catch(Exception e ){
+      //Questa eccezione di metodo non trovato secondo me va tradotta in una beaninfo exception (annotation dice che il field è un setter/getter ma il corrispettivo metodo non c'è)
+      //Credo che tutte le exception di reflection andrebbero wrappate in un'eccezzione di beaninfo.
+      e.printStackTrace();
+    }
   }
 
   private void fillMethods(Method[] allMethods){
@@ -36,7 +47,6 @@ public class BeanInfo{
     setters = new HashMap<String,Method>();
     for (Method m : allMethods){
       String name = m.getName();
-      System.out.println(name);
       if(name.contains("get")){
         getters.put(name.substring(3).toLowerCase() , m);
       }
@@ -64,18 +74,25 @@ public class BeanInfo{
     }
   }
 
-  private void fillMethods(Field[] allFields){
-    //getters = new HashMap<String,Method>();
-    //setters = new HashMap<String,Method>();
+  private void fillMethods(Field[] allFields) throws NoSuchMethodException {
+    getters = new HashMap<String,Method>();
+    setters = new HashMap<String,Method>();
     for (Field f : allFields) {
-	String name = f.getName();
-	if(f.isAnnotationPresent(Setter.class)) {
-		StringBuffer methodName = new StringBuffer("set");
-		methodName.append(name.substring(0, 1).toUpperCase());
-		methodName.append(name.substring(1).toLowerCase());
-		//setters.put(name.toLowerCase(), clazz.getMethod(methodName.toString(), f.getType()));
-		System.out.println(methodName);
-	}
+      String name = f.getName();
+      if(f.isAnnotationPresent(Setter.class)) {
+        StringBuffer methodName = new StringBuffer("set");
+        methodName.append(name.substring(0, 1).toUpperCase());
+        methodName.append(name.substring(1).toLowerCase());
+        setters.put(name.toLowerCase(), clazz.getMethod(methodName.toString(), f.getType()));
+      }
+      if(f.isAnnotationPresent(Getter.class)){
+        StringBuffer methodName = new StringBuffer();
+        if(f.getType().isAssignableFrom(Boolean.TYPE)) methodName.append("is");
+        else methodName.append("get");
+        methodName.append(name.substring(0, 1).toUpperCase());
+        methodName.append(name.substring(1).toLowerCase());
+        getters.put(name.toLowerCase(), clazz.getMethod(methodName.toString(), (Class<?>[]) null));
+      }
 
     }
   }
@@ -104,4 +121,8 @@ public class BeanInfo{
   public Object getInstance() throws java.lang.InstantiationException, java.lang.IllegalAccessException {
     return clazz.newInstance();
   }
+}
+
+class BeanException extends Exception {
+  private static final long serialVersionUID = 1L; 
 }
